04d7684941cd81e1e05c5951256de669166805f8
Zihadul Islam <zihadislam2429@gmail.com>
2025-05-19 17:46:40 +0600

    js-fundamentals: operators

---
+++ 01-fundamentals/operators.js
@@ -0,0 +1,31 @@
+// 3. OPERATORS => are used to perform different types of methematical and logical computations. examples = + * >
+
+// JavaScrpt Assignment ( = ) operator assigns a value to a variable
+let x = 10;
+let y = 90;
+
+// JavaScript Addition ( + ) adds numbers
+let z = x + y;
+
+// JavaScript Multiplication ( * ) multiplies numbers
+let multiply = x * y;
+
+// There are differnt types of javascript operators:
+/* 
+    - Arithmetic 
+    - Assignment 
+    - Comparison 
+    - String 
+    - Logical 
+    - Bitwise 
+    - Ternary 
+    - Type
+*/
+
+// Arithmetic +, -, *, **, /, %, ++, --
+// Assignment =, +=, -=, *=, /=, %=, **=
+// Comparison ==, ===, !=, !==, >, <, >=, <=, ?
+// String <, +, +=
+// Logical &&, ||, !
+// Type typeof, instanceof
+// Bitwise &, |, ^, ~, <<, >>, >>>
